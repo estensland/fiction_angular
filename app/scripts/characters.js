afa0cc192627f2e797e429f3b73a12ba394427c7
Eric Stensland <estensland88@gmail.com>
2014-10-30 18:50:50 -0500

    getting basics further down

--- app/scripts/characters.js
+++ app/scripts/characters.js
@@ -8,27 +8,41 @@ angular.module('myApp.characters', ['ngRoute'])
       controller: 'CharactersCtrl',
       resolve: {
         characters: ['$http', function($http) {
-          return $http.get('/api/characters.json').then(function(response) {
+          return $http.get('/api/people.json').then(function(response) {
             return response.data;
-          })
+          });
         }]
       }
     });
     $routeProvider.when('/characters/:id', {
       templateUrl: 'partials/characters/show.html',
-      controller: 'CharactersCtrl',
+      controller: 'CharacterCtrl',
       resolve: {
-        characters: ['$http', function($http) {
-          return $http.get('/api/characters.json').then(function(response) {
+        characters: ['$http','$routeParams', function($http, $routeParams) {
+          return $http.get('/api/people.json').then(function(response) {
             return response.data;
-          })
-        }]
+          });
+        }],
       }
     });
   }])
 
   .controller('CharactersCtrl', ['$scope', '$routeParams', 'characters', function($scope, $routeParams, characters) {
     $scope.title = 'Characters';
-    $scope.test = $routeParams['id'];
     $scope.characters = characters;
-}]);
+    $scope.param = $routeParams.id;
+}])
+  .controller('CharacterCtrl', ['$scope', '$routeParams', 'characters', function($scope, $routeParams, characters) {
+    $scope.title = 'Characters';
+    $scope.character = characters[$routeParams.id];
+    $scope.titleName = function(){
+        var title = ''
+        var bailicName = $scope.character.balicName;
+        var nativeName = $scope.character.nativeName;
+        var house = $scope.character.house;
+        if (bailicName !== '*Refuses*'){title = title + bailicName + " "; }
+        if (nativeName !== '*Same*'){title = title + nativeName + " ";}
+        if (house !== 'Non-Noble'){title = title + house;}
+        return title;
+      }();
+    }]);
