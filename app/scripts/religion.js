281ee6189a4b256b6dd499d07ecee28f4f2c9cd9
Eric Stensland <estensland88@gmail.com>
2014-11-02 22:39:18 -0600

    middle of adding new featurish things

---
+++ app/scripts/religion.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('myApp.religion', ['ngRoute'])
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/characters', {
+      templateUrl: 'partials/characters/',
+      controller: 'CharactersCtrl',
+      resolve: {
+        characters: ['$http', function($http) {
+          return $http.get('/api/people.json').then(function(response) {
+            return response.data;
+          });
+        }]
+      }
+    });
+    $routeProvider.when('/characters/:id', {
+      templateUrl: 'partials/characters/show.html',
+      controller: 'CharacterCtrl',
+      resolve: {
+        characters: ['$http','$routeParams', function($http, $routeParams) {
+          return $http.get('/api/people.json').then(function(response) {
+            return response.data;
+          });
+        }],
+      }
+    });
+  }])
+
+  .controller('CharactersCtrl', ['$scope', '$routeParams', 'characters', function($scope, $routeParams, characters) {
+    $scope.title = 'Characters';
+    $scope.characters = characters;
+    $scope.param = $routeParams.id;
+}])
+  .controller('CharacterCtrl', ['$scope', '$routeParams', 'characters', function($scope, $routeParams, characters) {
+    $scope.character = characters[$routeParams.id];
+    $scope.coatOfArms = "/api/images/" + $scope.character.coatOfArms;
+    $scope.title = function(){
+      var title = '';
+      var nobleOffice = $scope.character.nobleOffice;
+      var bailicName = $scope.character.bailicName;
+      var nativeName = $scope.character.nativeName;
+      var house = $scope.character.house;
+      var nickname = $scope.character.nickname;
+
+      if (nobleOffice !== undefined) {title = title + nobleOffice + " "; }
+      if (bailicName !== '*Refuses*') {title = title + bailicName + " "; }
+      if (nativeName !== '*Same*')    {title = title + nativeName + " ";}
+      if (house !== 'Non-Noble')      {title = title + house;}
+      if (nickname) {
+        if (nickname.match(/\(.*\)/)){
+          nickname = nickname.replace(/\(.*\)/, '');
+        }
+        title = title + " " + nickname;
+      }
+
+      return title;
+    }();
+  }]);
