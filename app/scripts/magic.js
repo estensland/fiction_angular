ee0227797d42e1b4010487ce77c397d4703468d2
Eric Stensland <estensland88@gmail.com>
2014-11-11 07:35:44 -0600

    continuing work on new components

---
+++ app/scripts/magic.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('myApp.magic', ['ngRoute'])
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/magic', {
+      templateUrl: 'partials/magic/',
+      controller: 'MagicsCtrl',
+      resolve: {
+        json_grab: ['$http','$routeParams', function($http, $routeParams) {
+          return $http.get('/api/magic.json').then(function(response) {
+            return response.data;
+          });
+        }],
+      }
+    });
+    $routeProvider.when('/magic/:name', {
+      templateUrl: 'partials/magic/show.html',
+      controller: 'MagicCtrl',
+      resolve: {
+        json_grab: ['$http','$routeParams', function($http, $routeParams) {
+          return $http.get('/api/magic.json').then(function(response) {
+            return response.data;
+          });
+        }],
+      }
+    });
+  }])
+
+  .controller('MagicsCtrl', ['$scope', 'json_grab', function($scope, json_grab) {
+    $scope.title = 'Magic';
+    $scope.magic = json_grab;
+}])
+  .controller('MagicCtrl', ['$scope', '$routeParams', 'json_grab', function($scope, $routeParams, json_grab) {
+    $scope.title = $routeParams.name;
+    $scope.json = json_grab;
+    $scope.magic = json_grab[$scope.title];
+    $scope.keys = ['name', 'translation', 'desc'];
+    $scope.blurb = $scope.magic.blurb;
+  }]);
