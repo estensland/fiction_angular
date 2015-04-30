70b05a9eb818877d961d94430be3be70b8a4badf
Eric Stensland <estensland88@gmail.com>
2015-04-29 21:35:46 -0500

    removed test

--- app/app.js
+++ app/app.js
@@ -4,13 +4,6 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.home',
-  'myApp.characters',
-  'myApp.magic',
-  'myApp.peerage',
-  'myApp.peoples',
-  'myApp.places',
-  'myApp.religion',
-  'myApp.weapons',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
